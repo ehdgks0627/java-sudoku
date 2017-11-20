bbb0098a4aecdd9b972480a03d91a76461b5b91d
sprout <ehdgks0627@naver.com>
2017-11-20 12:22:02 +0900

    init JFrame

--- src/com/sudoku/GameFrame.java
+++ src/com/sudoku/GameFrame.java
@@ -1,13 +1,33 @@
 package com.sudoku;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameFrame extends JFrame {
+    private JButton btnNewGame = new JButton("New Game");
+    private JButton btnShowAnswer = new JButton("Show Answer");
+    private JButton btnExit = new JButton("Exit");
+
+    private JPanel mainPanel = new JPanel();
+    private GamePanel gamePanel = new GamePanel();
+    private JPanel controlPanel = new JPanel();
+
     GameFrame() {
         setTitle("스도쿠 게임");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        this.add(new GamePanel());
+        controlPanel.setLayout(new GridLayout(3,1));
+        controlPanel.setSize(200,600);
+        controlPanel.add(btnNewGame);
+        controlPanel.add(btnShowAnswer);
+        controlPanel.add(btnExit);
+
+        mainPanel.setLayout(new BorderLayout());
+        mainPanel.add(gamePanel, BorderLayout.LINE_START);
+        mainPanel.add(controlPanel, BorderLayout.LINE_END);
+
+        add(mainPanel);
     }
+
 }
--- src/com/sudoku/GamePanel.java
+++ src/com/sudoku/GamePanel.java
@@ -1,9 +1,15 @@
 package com.sudoku;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GamePanel extends JPanel {
     GamePanel() {
+        setLayout(new GridLayout(9, 9));
 
+        for (int i = 0; i < 9 * 9; i++) {
+            JButton t = new JButton("" + i);
+            add(t);
+        }
     }
 }
